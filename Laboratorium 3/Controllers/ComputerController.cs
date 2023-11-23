bec1f922b9fc4fd90137db5010806a9fc7b60520
WiktorNiewiadomski <wiktor.niewiadomski3@gmail.com>
2023-11-23 16:42:32 +0100

    Lab 3

---
+++ Laboratorium 3/Controllers/ComputerController.cs
@@ -0,0 +1,106 @@
+﻿using Laboratorium_3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Laboratorium_3.Controllers
+{
+    public class ComputerController : Controller
+    {
+        static Dictionary<int, Computer> _computers = new Dictionary<int, Computer>();
+
+        public IActionResult Index()
+        {
+            return View(_computers);
+        }
+
+        [HttpGet]
+        public IActionResult Create() { return View(); }
+
+        [HttpPost]
+        public IActionResult Create(Computer model)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = _computers.Keys.Count != 0 ? _computers.Keys.Max() : 0;
+                model.Id = id + 1;
+                _computers.Add(model.Id, model);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+
+            if (_computers.Keys.Contains(id))
+            {
+                return View(_computers[id]);
+            }
+            else
+            {
+                return NotFound();
+            };
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Computer model)
+        {
+            if (ModelState.IsValid)
+            {
+                _computers[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+
+            if (_computers.Keys.Contains(id))
+            {
+                return View(_computers[id]);
+            }
+            else
+            {
+                return NotFound();
+            };
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Computer model)
+        {
+            if (ModelState.IsValid)
+            {
+                _computers.Remove(model.Id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+
+            if (_computers.Keys.Contains(id))
+            {
+                return View(_computers[id]);
+            }
+            else
+            {
+                return NotFound();
+            };
+        }
+    }
+}
---
+++ Laboratorium 3/Models/Computer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium_3.Models
+{
+    public class Computer
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Processor is required")]
+        public string Processor { get; set; }
+        [Required(ErrorMessage = "Storage is required")]
+        public string Storage { get; set; }
+        [Required(ErrorMessage = "GraphicsCard is required")]
+        public string GraphicsCard { get; set; }
+        [Required(ErrorMessage = "Manufacturer is required")]
+        public string Manufacturer { get; set; }
+        [Required(ErrorMessage = "DateOfManufacture is required")]
+        [DataType(DataType.DateTime, ErrorMessage = "It needs to be a date")]
+        public DateTime DateOfManufacture { get; set; }
+     }
+}
